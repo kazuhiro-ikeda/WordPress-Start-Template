c3d9404470876faba77f519dc321fa0be8c6656b
Kazuhiro Ikeda <info@moriad.jp>
2014-11-07 18:43:40 +0900

    revision
    コード整形

--- functions.php
+++ functions.php
@@ -1,63 +1,63 @@
 <?php
 	
 	// カスタムメニュー
-	register_nav_menus(
-		array(
-			'global' => 'グローバル' ,
-			'side-01' => 'サイド01' ,
-			'footer-01' => 'フッター01' ,
-			'sp-01' => 'スマホ01' ,
-			'sitemap-01' => 'サイトマップ01' ,
-			)   
-			);
+		register_nav_menus(
+			array(
+				'global' => 'グローバル' ,
+				'side-01' => 'サイド01' ,
+				'footer-01' => 'フッター01' ,
+				'sp-01' => 'スマホ01' ,
+				'sitemap-01' => 'サイトマップ01' ,
+				)   
+				);
 
 	//エディタのスタイル
-	add_editor_style('editor-style.css');
+		add_editor_style('editor-style.css');
 
 	// サイトIDタグ
-	function diverge_site_id() {
-		if (is_front_page()) {
-			echo "h1";
-			} else {
-				echo "div";
+		function diverge_site_id() {
+			if (is_front_page()) {
+				echo "h1";
+				} else {
+					echo "div";
+					}
 				}
-			}
-	function diverge_tagline() {
-		if (is_front_page()) {
-			echo "h2";
-			} else {
-				echo "div";
+		function diverge_tagline() {
+			if (is_front_page()) {
+				echo "h2";
+				} else {
+					echo "div";
+					}
 				}
-			}
-
+	
 	//even and odd addc class
-	function oddeven_post_class ( $classes ) {
-		global $current_class;
-		$classes[] = $current_class;
-		$current_class = ($current_class == 'odd') ? 'even' : 'odd';
-		return $classes;
-	}
-	add_filter ( 'post_class' , 'oddeven_post_class' );
-		global $current_class;
-		$current_class = 'odd';
+		function oddeven_post_class ( $classes ) {
+			global $current_class;
+			$classes[] = $current_class;
+			$current_class = ($current_class == 'odd') ? 'even' : 'odd';
+			return $classes;
+			}
+		add_filter ( 'post_class' , 'oddeven_post_class' );
+			global $current_class;
+			$current_class = 'odd';
 
 	//最初と最後の要素
-	function isFirst(){
-		global $wp_query;
-		return ($wp_query->current_post === 0);
-	}
-	function isLast(){
-		global $wp_query;
-		return ($wp_query->current_post+1 === $wp_query->post_count);
-	}
+		function isFirst(){
+			global $wp_query;
+			return ($wp_query->current_post === 0);
+			}
+		function isLast(){
+			global $wp_query;
+			return ($wp_query->current_post+1 === $wp_query->post_count);
+			}
 	
 	// タイトルタグのテキストを出力
-	function full_title() {
-		if (!is_front_page()) {
-			echo trim(wp_title('', false)) . " | ";
-			} 
-			bloginfo('name');
-		}
+		function full_title() {
+			if (!is_front_page()) {
+				echo trim(wp_title('', false)) . " | ";
+				} 
+				bloginfo('name');
+			}
 	
 	//body にスラッグを追加
 		function getLoopCount(){
@@ -78,8 +78,8 @@
 		
 	//アイキャッチ画像の有効化
 		add_theme_support('post-thumbnails');
-		add_image_size( 'image-l', 640, 480, true ); 
-		add_image_size( 'image-m', 320, 240, true ); 
+		add_image_size( 'image-l', 640, 480, true );
+		add_image_size( 'image-m', 320, 240, true );
 		add_image_size( 'image-s', 160, 120, true );
 		
 	//カテゴリをID順に取得
@@ -89,7 +89,7 @@
 		}
 		add_filter( 'get_the_categories', 'get_the_category_orderby_parent' );
 
-	//不要なタグを表示しない
+	//不要なメタタグを表示しない
 		remove_action('wp_head', 'wp_generator');
 
 		remove_action('wp_head', 'rsd_link');
