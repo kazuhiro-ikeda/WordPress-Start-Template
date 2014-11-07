0fdf5800e87a78d0b5a6dacdfb736e2338beb18a
Kazuhiro Ikeda <info@moriad.jp>
2014-11-07 18:08:40 +0900

    initial commit
    page.php と CSS 追加
    
    テンプレートに#main と WPの ポストクラス関数を追加

---
+++ page.php
@@ -0,0 +1,29 @@
+<?php get_header(); ?>
+
+<?php if (wp_is_mobile()) : ?>
+			<!-- mobile -->
+			<article id="main" <?php post_class(); ?>>
+			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+				<h1><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h1>
+				<?php remove_filter ('the_content', 'wpautop'); ?>
+				<?php the_content(); ?>
+			<?php endwhile; else: ?>
+				<p>お探しの記事はありません。</p>
+			<?php endif; ?>
+			</article></article><!-- /#main post_class -->
+			<?php else : ?>
+			
+			<!-- pc -->
+			<article id="main" <?php post_class(); ?>>
+			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+				<h1><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h1>
+				<?php remove_filter ('the_content', 'wpautop'); ?>
+				<?php the_content(); ?>
+			<?php endwhile; else: ?>
+				<p>お探しの記事はありません。</p>
+			<?php endif; ?>
+			</article><!-- /#main post_class -->
+			<?php  endif; ?>
+			<!-- /if_mobile -->
+
+<?php get_footer(); ?>
