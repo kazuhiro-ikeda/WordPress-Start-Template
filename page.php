7d57bac3d44f15534ffae66f8a1601827696e13c
Kazuhiro Ikeda <info@moriad.jp>
2014-11-22 18:29:52 +0900

    revision
    バグフィックス

--- page.php
+++ page.php
@@ -7,31 +7,33 @@
 <?php endwhile; else: ?>
 	<p>お探しの記事はありません。</p>
 <?php endif; ?>
-</article><!-- /#main post_class -->
-<?php else : ?>
 
+</article><!-- /#main post_class -->
 
 <?php if (wp_is_mobile()) : ?>
-	<!-- mobile -->
-	
-	<!-- pc -->
-	<div id="sidebar">
-		<?php wp_nav_menu(
-			array(
-				'theme_location' => 'side' ,
-				'container'       => 'nav',
-				'container_id'    => '',
-				'container_class' => 'cl',
-				'menu_id'         => 'side-nav',
-				'menu_class'      => ''
-				//スタイル
-				//<nav class="cl">
-				//<ul id="side-nav">
-				//<li><a href=""></a></li>
-				//</ul>
-				//</nav>
-		)); ?>	
-	</div><!-- /#sidebar -->
-	<!-- /if_mobile -->
+			<!-- mobile -->
+			
+	<?php else : ?>
+			<!-- pc -->
+			<div id="sidebar">
+				<?php wp_nav_menu(
+					array(
+						'theme_location' => 'side' ,
+						'container'       => 'nav',
+						'container_id'    => '',
+						'container_class' => 'cl',
+						'menu_id'         => 'side-nav',
+						'menu_class'      => ''
+						//スタイル
+						//<nav class="cl">
+						//<ul id="side-nav">
+						//<li><a href=""></a></li>
+						//</ul>
+						//</nav>
+				)); ?>	
+			</div><!-- /#sidebar -->
+			
+	<?php  endif; ?>
+			<!-- /if_mobile -->
 
 <?php get_footer(); ?>
