080f49cd1117933288ed1e45666a4dc65225ac45
Kazuhiro Ikeda <info@moriad.jp>
2015-02-06 13:46:43 +0900

    404 (index.php) 修正

--- index.php
+++ index.php
@@ -1,15 +1,14 @@
 <?php get_header(); ?>
 
 <div class="screen">
-	index
-</div>
-<!-- /.screen -->
 
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 	<h1><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h1>
 	<?php the_content(); ?>
 <?php endwhile; else: ?>
-<p style="text-align:center; font-size:24px; font-weight:bold; color:#ddd; margin:100px auto;">お探しの記事は準備中です。<br>近日中に公開となります。</p>
+	<p style="text-align:center; font-size:24px; font-weight:bold; color:#ddd; margin:300px auto;">お探しの記事は存在しないか準備中です。</p>
 <?php endif; ?>
 
+</div>
+<!-- /.screen -->
 <?php get_footer(); ?>
