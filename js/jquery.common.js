6895b2e66e432eb8c8d1ff584f7ee2488b469910
Kazuhiro Ikeda <info@moriad.jp>
2015-03-06 09:53:24 +0900

    rollover

--- js/jquery.common.js
+++ js/jquery.common.js
@@ -9,9 +9,9 @@ $(function(){
 			$( ".rollover img" ).mouseover(function(){
 				$(this).attr( "src",$(this).attr( "src" ).replace(/^(.+)(\.[a-z]+)$/, "$1_on$2" ));
 			}).mouseout(function(){
-				$(this).attr( " src",$(this).attr( " src" ).replace(/^(.+)_on(\.[a-z]+)$/, "$1$2" ));
+				$(this).attr( "src",$(this).attr( "src" ).replace(/^(.+)_on(\.[a-z]+)$/, "$1$2" ));
 			}).each(function(){
-				$( " <img>" ).attr( " src",$(this).attr( " src" ).replace(/^(.+)(\.[a-z]+)$/, "$1_on$2" ));
+				$( "<img>" ).attr( "src",$(this).attr( "src" ).replace(/^(.+)(\.[a-z]+)$/, "$1_on$2" ));
 			});
 		
 	//スムーススクロール
