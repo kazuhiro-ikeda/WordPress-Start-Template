fd7b47740053dba4aa3641d9577184b53688d82e
Kazuhiro Ikeda <info@moriad.jp>
2015-03-13 22:27:09 +0900

    コメント調整

--- js/jquery.adjust.js
+++ js/jquery.adjust.js
@@ -4,17 +4,6 @@ Author : Kazuhiro Ikeda
 
 $(function(){
 	
-	//画像のプリロード
-		
-		    jQuery.preloadImages = function(){
-		        for(var i = 0; i<arguments.length; i++){
-		            jQuery("<img>").attr("src", arguments[i]);
-		        }
-		    };
-		    $.preloadImages(
-		    "wp-content/theme/epi-rank/images/common/xxx.png" ,
-		    "wp-content/theme/epi-rank/images/common/xxx.png" ,
-		    "wp-content/theme/epi-rank/images/common/xxx.png" 
-		    );
+	//
 	
 });// END TAG
