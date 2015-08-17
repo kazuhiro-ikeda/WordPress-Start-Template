d390b13f0f8a05bd6c87075859a6b18132b8d39c
Kazuhiro Ikeda <info@moriad.jp>
2015-08-17 13:58:57 +0900

    page.php

--- page.php
+++ page.php
@@ -11,7 +11,7 @@
 ?>
 
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-<?php remove_filter ('the_content', 'wpautop'); ?>
+<?php remove_filter ( 'the_content', 'wpautop' ); ?>
 <?php the_content(); ?>
 <?php //advanced custom fields プラグイン the_field("prefecture", $post->ID); ?>
 	
