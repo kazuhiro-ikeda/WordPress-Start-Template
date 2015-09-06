19e2e59335eee914bd08c7198ad98f80d077cea4
Kazuhiro Ikeda <info@moriad.jp>
2015-09-07 03:35:25 +0900

    sticky nav

--- js/jquery.common.js
+++ js/jquery.common.js
@@ -125,6 +125,40 @@ $(function(){
 		</script>
 	*/
 	
+	//スティッキーヘッダー
+	$('#hoge').each(function () {
+
+        var $window = $(window), // ウィンドウを jQuery オブジェクト化
+            $header = $(this),   // ヘッダーを jQuery オブジェクト化
+            // ヘッダーのデフォルト位置を取得
+            headerOffsetTop = $header.offset().top;
+
+        // ウィンドウのスクロールイベントを監視
+        // (ウィンドウがスクロールするごとに処理を実行する)
+        $window.on('scroll', function () {
+            // ウィンドウのスクロール量をチェックし、
+            // ヘッダーのデフォルト位置を過ぎていればクラスを付与、
+            // そうでなければ削除
+            if ($window.scrollTop() > headerOffsetTop) {
+                $header.addClass('sticky');
+            } else {
+                $header.removeClass('sticky');
+            }
+        });
+
+        // ウィンドウのスクロールイベントを発生させる
+        // (ヘッダーの初期位置を調整するため)
+        $window.trigger('scroll');
+
+    });
+    /*	
+	    CSS
+	    #hoge.sticky {
+		    position: fixed;
+		    top: 0;
+		}
+	*/
+	
 	//MW WP Form 必須
 	$( '#mw_wp_form_mw-wp-form-識別ナンバー select option[value=""]' ).html( 'お問合せ内容を選択' );
 	
