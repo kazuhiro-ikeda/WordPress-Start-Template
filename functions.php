5d8364324347dfcc41232951c732c9db879ec22a
Kazuhiro Ikeda <info@moriad.jp>
2015-10-20 13:40:42 +0900

    add plugin

--- functions.php
+++ functions.php
@@ -1,26 +1,5 @@
 <?php
 	
-	/*
-	//相対パスに変更　阪本さん専用
-		class relative_URI {
-				    function relative_URI() {
-				        add_action('get_header', array(&$this, 'get_header'), 1);
-				        add_action('wp_footer', array(&$this, 'wp_footer'), 99999);
-				    }
-				    function replace_relative_URI($content) {
-				        $home_url = trailingslashit(get_home_url('/'));
-				        return str_replace($home_url, '/', $content);
-				    }
-				    function get_header(){
-				        ob_start(array(&$this, 'replace_relative_URI'));
-				    }
-				    function wp_footer(){
-				        ob_end_flush();
-				    }
-				}
-		new relative_URI();
-		*/
-
 	//タブレットをモバイルから除外
 		function is_mobile() {
 		    $useragents = array(
