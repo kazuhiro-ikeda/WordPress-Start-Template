664c49e7e4a5b712593e674e624468c4cabb4f37
Kazuhiro Ikeda <info@moriad.jp>
2015-12-17 16:03:48 +0900

    修正

--- footer.php
+++ footer.php
@@ -3,12 +3,12 @@
 </footer>
 
 <!--jQuery --> 
-<script src="<?php bloginfo(template_url);?>/js/jquery.common.js"></script>
-<script src="<?php bloginfo(template_url);?>/js/jquery.height.js"></script>
-<script src="<?php bloginfo(template_url);?>/js/lightbox.js"></script>
-<script src="<?php bloginfo(template_url);?>/js/jquery.adjust.js"></script>
-<script src="<?php bloginfo(template_url);?>/js/jquery.mousewheel.js"></script>
-<script src="<?php bloginfo(template_url);?>/js/jquery.jscrollpane.min.js"></script>
+<script src="<?php bloginfo('template_url');?>/js/jquery.common.js"></script>
+<script src="<?php bloginfo('template_url');?>/js/jquery.adjust.js"></script>
+<script src="<?php bloginfo('template_url');?>/js/jquery.height.js"></script>
+<script src="<?php bloginfo('template_url');?>/js/lightbox.js"></script>
+<script src="<?php bloginfo('template_url');?>/js/jquery.mousewheel.js"></script>
+<script src="<?php bloginfo('template_url');?>/js/jquery.jscrollpane.min.js"></script>
 <script>
 	$(function() {
 		$('.scroll-pane').jScrollPane({
