74958aeee3a733d359869223d229831380bb6c32
Kazuhiro Ikeda <info@moriad.jp>
2017-02-03 22:19:29 +0900

    functions

--- functions.php
+++ functions.php
@@ -1,5 +1,15 @@
 <?php
 	
+	//スラッグ強制
+	function auto_post_slug( $slug, $post_ID, $post_status, $post_type ) {
+	    if ( preg_match( '/(%[0-9a-f]{2})+/', $slug ) ) {
+	        $slug = utf8_uri_encode( $post_type ) . '-' . $post_ID;
+	    }
+	    return $slug;
+	}
+	add_filter( 'wp_unique_post_slug', 'auto_post_slug', 10, 4  );
+	
+	
 	//ビジュアルエディタの非表示
 	function disable_visual_editor_in_page(){
 		global $typenow;
