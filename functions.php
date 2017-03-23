1df0ea927175c53cc45d4ac0d8ca8882021461c4
Kazuhiro Ikeda <info@moriad.jp>
2017-03-23 17:08:43 +0900

    functions

--- functions.php
+++ functions.php
@@ -1,5 +1,33 @@
 <?php
 	
+	//カテゴリ選択制限
+	add_action( 'admin_print_footer_scripts', 'limit_category_select' );
+	function limit_category_select() {
+		?>
+		<script type="text/javascript">
+			jQuery(function($) {
+				// 投稿画面のカテゴリー選択を制限
+				var cat_checklist = $('.categorychecklist input[type=checkbox]');
+				cat_checklist.click( function() {
+					$(this).parents('.categorychecklist').find('input[type=checkbox]').attr('checked', false);
+					$(this).attr('checked', true);
+				});
+				
+				// クイック編集のカテゴリー選択を制限
+				var quickedit_cat_checklist = $('.cat-checklist input[type=checkbox]');
+				quickedit_cat_checklist.click( function() {
+					$(this).parents('.cat-checklist').find('input[type=checkbox]').attr('checked', false);
+					$(this).attr('checked', true);
+				});
+			    
+				$('.categorychecklist>li:first-child, .cat-checklist>li:first-child').before('<p style="padding-top:5px;">カテゴリーは1つしか選択できません</p>');
+			});
+		</script>
+		<?php
+	}
+	//カテゴリ選択制限ここまで
+	
+	
 	//スラッグ強制
 	function auto_post_slug( $slug, $post_ID, $post_status, $post_type ) {
 	    if ( preg_match( '/(%[0-9a-f]{2})+/', $slug ) ) {
