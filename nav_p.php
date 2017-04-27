aae1134aa2bbde2d8d96d34b1e36ea0c1e9a1056
Kazuhiro Ikeda <info@moriad.jp>
2017-04-27 19:14:59 +0900

    nav

---
+++ nav_p.php
@@ -0,0 +1,26 @@
+
+<ul class="n3_package">
+	<li><a href="<?php bloginfo( 'url' ); ?>/genre/style_a">スタイルA</a></li>
+	<li><a href="<?php bloginfo( 'url' ); ?>/genre/style_b">スタイルB</a></li>
+	<li><a href="<?php bloginfo( 'url' ); ?>/genre/style_c">スタイルC</a></li>
+</ul>
+
+<ul class="n3_package alternately">
+	<li><a href="<?php bloginfo( 'url' ); ?>/genre/style_a">スタイルA</a></li>
+	<li><a href="<?php bloginfo( 'url' ); ?>/genre/style_b">スタイルB</a></li>
+	<li><a href="<?php bloginfo( 'url' ); ?>/genre/style_c">スタイルC</a></li>
+</ul>
+
+<ul class="n4_package">
+	<li><a href="<?php bloginfo( 'url' ); ?>/genre/style_a">スタイルA</a></li>
+	<li><a href="<?php bloginfo( 'url' ); ?>/genre/style_b">スタイルB</a></li>
+	<li><a href="<?php bloginfo( 'url' ); ?>/genre/style_c">スタイルC</a></li>
+	<li><a href="<?php bloginfo( 'url' ); ?>/genre/style_d">スタイルC</a></li>
+</ul>
+
+<ul class="n4_package alternately">
+	<li><a href="<?php bloginfo( 'url' ); ?>/area/area_a">エリアA</a></li>
+	<li><a href="<?php bloginfo( 'url' ); ?>/area/area_b">エリアB</a></li>
+	<li><a href="<?php bloginfo( 'url' ); ?>/area/area_c">エリアC</a></li>
+	<li><a href="<?php bloginfo( 'url' ); ?>/area/area_d">エリアD</a></li>
+</ul>
--- taxonomy-genre.php
+++ taxonomy-genre.php
@@ -20,19 +20,7 @@
 
 	<section id="loop">
 		
-		<ul class="n4_package">
-			<li><a href="<?php bloginfo( 'url' ); ?>/genre/style_a">受付・事務</a></li>
-			<li><a href="<?php bloginfo( 'url' ); ?>/genre/style_b">総務・経理</a></li>
-			<li><a href="<?php bloginfo( 'url' ); ?>/genre/style_c">医療事務</a></li>
-			<li><a href="<?php bloginfo( 'url' ); ?>/genre/style_d">その他</a></li>
-		</ul>
-		
-		<ul class="n4_package alternately">
-			<li><a href="<?php bloginfo( 'url' ); ?>/area/area_a">名古屋市内</a></li>
-			<li><a href="<?php bloginfo( 'url' ); ?>/area/area_b">名古屋市外</a></li>
-			<li><a href="<?php bloginfo( 'url' ); ?>/area/area_c">岐阜</a></li>
-			<li><a href="<?php bloginfo( 'url' ); ?>/area/area_d">三重</a></li>
-		</ul>
+		<?php get_template_part( 'nav_p' ); ?>
 
 		<div class="items">
 		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
