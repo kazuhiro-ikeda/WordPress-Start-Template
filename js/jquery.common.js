395d2d8ce13f69f6cc06aed347ad27f664068d5b
Kazuhiro Ikeda <info@moriad.jp>
2018-02-28 14:01:16 +0900

    query

--- js/jquery.common.js
+++ js/jquery.common.js
@@ -3,6 +3,27 @@ Author : Kazuhiro Ikeda
 */
 
 $(function(){
+	
+	//ふわっと
+	$(function() {
+	    var topBtn = $('#pagetop');    
+	    topBtn.hide();
+	    //スクロールが100に達したらボタン表示
+	    $(window).scroll(function () {
+	        if ($(this).scrollTop() > 300) {
+	            topBtn.fadeIn();
+	        } else {
+	            topBtn.fadeOut();
+	        }
+	    });
+	    //スクロールしてトップ
+	    topBtn.click(function () {
+	        $('body,html').animate({
+	            scrollTop: 0
+	        }, 500);
+	        return false;
+	    });
+	});
 		
 	//ロールオーバー
 		
