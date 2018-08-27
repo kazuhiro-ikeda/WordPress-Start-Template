3dbf1e9bcde3228c33086c60926a5e7747789efe
Kazuhiro Ikeda <info@moriad.jp>
2018-08-27 14:23:11 +0900

    slider

--- footer_p.php
+++ footer_p.php
@@ -50,7 +50,18 @@
 	
 </nav>
 
-<!--jQuery --> 
+<!--jQuery -->
+<script src="<?php bloginfo('template_url');?>/js/glide.js"></script>
+<script>
+    $("#Glide").glide({
+        type: "carousel"
+    });
+    
+     $("#Glide_s").glide({
+        type: "carousel"
+    });
+    
+</script>
 <script src="<?php bloginfo('template_url');?>/js/jquery.common.js"></script>
 <script src="<?php bloginfo('template_url');?>/js/jquery.adjust.js"></script>
 <script src="<?php bloginfo('template_url');?>/js/jquery.height.js"></script>
@@ -66,41 +77,6 @@
 	});
 </script>
 <script src="<?php bloginfo('template_url');?>/js/lightbox.js"></script>
-<script src="<?php bloginfo('template_url');?>/js/jquery.mousewheel.js"></script>
-<script src="<?php bloginfo('template_url');?>/js/jquery.jscrollpane.min.js"></script>
-<script>
-	$(function() {
-		$('.scroll-pane').jScrollPane({
-			showArrows:false,
-			scrollbarWidth: 50,
-			scrollbarMargin:0
-		});
-	});
-</script>
-<script src="<?php bloginfo('template_url');?>/js/jquery.bxslider.js"></script>
-<script>
-$(function(){
-	$('#bxslider__items').bxSlider({
-		auto: true,
-		slideWidth: 1000,
-		minSlides: 3,
-		maxSlides: 3,
-		moveSlides: 1,
-		slideMargin: 10,
-	});
-});
-</script>
-<script src="<?php bloginfo('template_url');?>/js/glide.js"></script>
-<script>
-    $("#Glide").glide({
-        type: "carousel"
-    });
-    
-     $("#Glide_s").glide({
-        type: "carousel"
-    });
-    
-</script>
 
 <?php wp_footer(); ?>
 
