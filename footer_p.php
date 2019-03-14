049a2bc3c913d74d52e5fc2c8843052a1a6e403e
Kazuhiro Ikeda <info@moriad.jp>
2019-03-14 13:12:10 +0900

    footer

--- footer_p.php
+++ footer_p.php
@@ -28,27 +28,31 @@
 	
 </div>
 
-<nav id="nav_foot">
-	<?php if(is_singular( 'case' )): ?>
+<div class="block_s">
+	<nav id="nav_foot">
+		<?php if(is_singular( 'case' )): ?>
+	
+		<div class="inner_case">
+			<a onclick="gtag('event', 'click', {'event_category': 'telephone','event_label': 'telclick', 'value': location.href});" href="tel:xxx"><i class="fa fa-phone" aria-hidden="true"></i>TEL応募</a>
+			<a onclick="gtag('event', 'click', {'event_category': 'oubobanner','event_label': 'ouboclick', 'value': location.href});" href="<?php echo esc_url(home_url('/')); ?>?page_id=55&post_id=<?php echo $post->ID; ?>"><i class="fa fa-envelope-o" aria-hidden="true"></i>WEB応募</a>
+			<a href="#requirements">募集詳細を見る</a>
+			
+		</div>
+		<!-- /.inner_case -->
+		
+		<?php else: ?>
+		<div class="inner_other">
+			<a href="#"><i class="fa fa-angle-up" aria-hidden="true"></i></a>
+			
+		</div>
+		<!-- /.inner_other -->
+		
+		<?php endif; ?>
+		
+	</nav>
 
-	<div class="inner_case">
-		<a onclick="gtag('event', 'click', {'event_category': 'telephone','event_label': 'telclick', 'value': location.href});" href="tel:xxx"><i class="fa fa-phone" aria-hidden="true"></i>TEL応募</a>
-		<a onclick="gtag('event', 'click', {'event_category': 'oubobanner','event_label': 'ouboclick', 'value': location.href});" href="<?php echo esc_url(home_url('/')); ?>?page_id=55&post_id=<?php echo $post->ID; ?>"><i class="fa fa-envelope-o" aria-hidden="true"></i>WEB応募</a>
-		<a href="#requirements">募集詳細を見る</a>
-		
-	</div>
-	<!-- /.inner_case -->
-	
-	<?php else: ?>
-	<div class="inner_other">
-		<a href="#"><i class="fa fa-angle-up" aria-hidden="true"></i></a>
-		
-	</div>
-	<!-- /.inner_other -->
-	
-	<?php endif; ?>
-	
-</nav>
+</div>
+<!-- /.block_s -->
 
 <!--jQuery -->
 <script src="<?php bloginfo('template_url');?>/js/glide.js"></script>
