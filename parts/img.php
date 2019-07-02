35cc8d16983b16ec96641f7474d32adb22981f38
Kazuhiro Ikeda <info@tamias.co.jp>
2019-07-02 23:48:16 +0900

    img

--- parts/img.php
+++ parts/img.php
@@ -4,19 +4,15 @@ if( !empty($image) ):
 
 	// vars
 	$url = $image['url'];
-	$title = $image['title'];
 	$alt = $image['alt'];
-	$caption = $image['caption'];
 
 	// thumbnail
-	$size = 'functions.phpで定義';
+	$size = 'hogehoge';
 	$thumb = $image['sizes'][ $size ];
-	$width = $image['sizes'][ $size . '-width' ];
-	$height = $image['sizes'][ $size . '-height' ];
 
 	 ?>
 	 
-	<img src="<?php echo $thumb; ?>" alt="<?php echo $alt; ?>" width="<?php echo $width; ?>" height="<?php echo $height; ?>" />
+<figure><img src="<?php echo $thumb; ?>" alt="<?php echo $alt; ?>"></figure>
 	
 <?php else: ?>
 		
