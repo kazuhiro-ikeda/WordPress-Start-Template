50b43b572ed57936fc78141949f270c9ab30ec5e
Kazuhiro Ikeda <info@tamias.co.jp>
2019-08-05 21:16:30 +0900

    404

--- 404.php
+++ 404.php
@@ -2,7 +2,7 @@
 
 <div id="main" <?php post_class(); ?> role="main">
 	<div id="page_404">
-		<p class="text_404"><?php the_field( "text_404", $post->ID); ?></p>
+		<p class="text_404"><?php the_field( "text_404", 54); ?></p>
 		
 		<a class="btn_top_404" href="<?php echo home_url(); ?>/">トップページへ</a>
 		
