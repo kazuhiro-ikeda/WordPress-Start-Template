3f8606bb2a073e10e503d92101c0019212caf0bc
Kazuhiro Ikeda <info@tamias.co.jp>
2019-12-21 18:48:53 +0900

    form

--- page-contact_thanks.php
+++ page-contact_thanks.php
@@ -24,16 +24,16 @@
 			<?php remove_filter ( 'the_content', 'wpautop' ); ?>
 			<?php the_content(); ?>
 			
+			<?php endwhile; else: ?>
+				<p style="text-align:center; font-size:24px; font-weight:bold; color:#ddd; margin:100px auto; line-height:200%;">お探しの記事は準備中です。<br>近日中に公開となります。</p>
+				
+			<?php endif; ?>
+			
 		</div>
 		<!-- /.inner -->
 		
 	</section>
 	<!-- /#entry -->
-
-<?php endwhile; else: ?>
-	<p style="text-align:center; font-size:24px; font-weight:bold; color:#ddd; margin:100px auto; line-height:200%;">お探しの記事は準備中です。<br>近日中に公開となります。</p>
-	
-<?php endif; ?>
 
 </div><!-- /#main post_class -->
 
