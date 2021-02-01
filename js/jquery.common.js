aa44c2ba0b5786fc7f1daa119850dd5c8c824171
Kazuhiro Ikeda <info@tamias.co.jp>
2021-02-02 04:05:28 +0900

    444

--- js/jquery.common.js
+++ js/jquery.common.js
@@ -5,24 +5,22 @@ Author : Kazuhiro Ikeda
 $(function(){
 	
 	//ふわっとpagetop
-	$(function() {
-	    var topBtn = $('hoge');    
-	    topBtn.hide();
-	    //スクロールが100に達したらボタン表示
-	    $(window).scroll(function () {
-	        if ($(this).scrollTop() > 300) {
-	            topBtn.fadeIn();
-	        } else {
-	            topBtn.fadeOut();
-	        }
-	    });
-	    //スクロールしてトップ
-	    topBtn.click(function () {
-	        $('body,html').animate({
-	            scrollTop: 0
-	        }, 500);
-	        return false;
-	    });
+	var topBtn = $('hoge');
+	topBtn.hide();
+	//スクロールが100に達したらボタン表示
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 300) {
+			topBtn.fadeIn();
+		} else {
+			topBtn.fadeOut();
+		}
+	});
+	//スクロールしてトップ
+	topBtn.click(function () {
+		$('body,html').animate({
+			scrollTop: 0
+		}, 500);
+		return false;
 	});
 	
 	//スティッキーヘッダー
