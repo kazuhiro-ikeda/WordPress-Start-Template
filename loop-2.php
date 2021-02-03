b27c913e58f288f68175c65150c8125c83db882f
Kazuhiro Ikeda <info@tamias.co.jp>
2021-02-04 00:41:55 +0900

    別スタイルのループ

--- loop-2.php
+++ loop-2.php
@@ -31,82 +31,52 @@
 		<div class="inner_box">
 			<div class="data">
 				<h2 class="title"><?php the_field( "title_case", $post->ID); ?></h2>
-				
-				<table class="info_table">
-					<tr>
-						<th><?php the_field( "title_case", $post->ID); ?></th>
-						<td><figure>
-							<?php 
-								$image = get_field( 'img_main', $post->ID );
-								if( !empty($image) ): 
-								
-								// vars
-								$url = $image['url'];
-								$alt = $image['alt'];
-							
-								// thumbnail
-								$size = '案件';
-								$thumb = $image['sizes'][ $size ];
-							
-								 ?>
-								 
-							<img src="<?php echo $thumb; ?>" alt="<?php echo $alt; ?>">
-							
-							<?php else: ?>
-							<img src="<?php bloginfo( 'template_url' ); ?>/images/case/img.jpg" alt="">
-							
-							<?php endif; ?>
-							
-						</figure></td>
-					</tr>
-				</table>
-				
-			
-			<table class="data_table">
-				<?php 
-					$detail = get_field('detail');
-					if( !empty($detail) ):
-				?>
-				<tr>
-					<th>仕事内容</th>
-					<td><?php 
-							        $text = mb_substr(get_field('detail'),0 ,60 ,'utf-8'); 
-							        $str = $text;
-														echo strip_tags( $str, '<br>');
-							        
-							        $number = mb_strlen(get_field('detail'));
-							        
-							        if($number > 60) {
-								        echo '…';
-							        }
-							?></td>
-				</tr>
-				
-				<?php endif; ?>
-				
-				<?php 
-					$place_archive = get_field('place_archive');
-					if( !empty($place_archive) ):
-				?>
-				<tr>
-					<th>勤務地</th>
-					<td><?php the_field( "place_archive", $post->ID); ?></td>
-				</tr>
-				
-				<?php endif; ?>
-				
-				<?php 
-					$salary_archive = get_field('salary_archive');
-					if( !empty($salary_archive) ):
-				?>
-				<tr>
-					<th>給与</th>
-					<td><?php the_field( "salary_archive", $post->ID); ?></td>
-				</tr>
-				
-				<?php endif; ?>
-				
-			</table>
+
+                <table class="data_table">
+                    <?php
+                        $detail = get_field('detail');
+                        if( !empty($detail) ):
+                    ?>
+                    <tr>
+                        <th>仕事内容</th>
+                        <td><?php
+                                        $text = mb_substr(get_field('detail'),0 ,60 ,'utf-8');
+                                        $str = $text;
+                                                            echo strip_tags( $str, '<br>');
+
+                                        $number = mb_strlen(get_field('detail'));
+
+                                        if($number > 60) {
+                                            echo '…';
+                                        }
+                                ?></td>
+                    </tr>
+
+                    <?php endif; ?>
+
+                    <?php
+                        $place_archive = get_field('place_archive');
+                        if( !empty($place_archive) ):
+                    ?>
+                    <tr>
+                        <th>勤務地</th>
+                        <td><?php the_field( "place_archive", $post->ID); ?></td>
+                    </tr>
+
+                    <?php endif; ?>
+
+                    <?php
+                        $salary_archive = get_field('salary_archive');
+                        if( !empty($salary_archive) ):
+                    ?>
+                    <tr>
+                        <th>給与</th>
+                        <td><?php the_field( "salary_archive", $post->ID); ?></td>
+                    </tr>
+
+                    <?php endif; ?>
+
+                </table>
 				
 			</div>
 			<!-- /.data -->
